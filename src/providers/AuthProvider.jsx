2896854e9037fc28982e98dd4cd9e66abfb38a74
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-06-07 11:55:05 +0600

    AuthContext and useAuth Hook created

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import React, { createContext } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+
+            // TODO: get and set token and remove and set to localStorage
+            // if (currentUser) {
+            //     axios.post('serverLink+/jwt', { email: currentUser.email })
+            //         .then(data => {
+            //             // console.log(data.data.token)
+            //             localStorage.setItem('access-token', data.data.token)
+            //             setLoading(false);
+            //         })
+            // }
+            // else {
+            //     localStorage.removeItem('access-token')
+            // }
+
+
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        googleSignIn,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,6 +5,7 @@ import {
 import Home from "../Pages/Home/Home";
 import Instructors from "../Pages/Instructors/Instructors";
 import Banner from "../Pages/Banner/Banner";
+import Register from "../Pages/Register/Register";
 
 
 const router = createBrowserRouter([
@@ -21,7 +22,8 @@ const router = createBrowserRouter([
                 element: <Instructors></Instructors>
             },
             {
-
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     },
