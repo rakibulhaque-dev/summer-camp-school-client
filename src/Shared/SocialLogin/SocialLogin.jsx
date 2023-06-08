f05b1b00b67bacd0274069f067e6cd0082432657
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-06-08 16:03:54 +0600

    Redirect from social login

--- src/Shared/SocialLogin/SocialLogin.jsx
+++ src/Shared/SocialLogin/SocialLogin.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import useAuth from '../../hooks/useAuth';
 import socialImg from '../../../src/assets/banner/socialia.png';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
 const SocialLogin = () => {
     const { googleSignIn } = useAuth();
 
     const handleGoogleSignIn = () => {
+        const location = useLocation();
+        const navigate = useNavigate();
+        const from = location.state?.from?.pathname || '/';
+
         googleSignIn()
             .then(result => {
-                console.log(result.user)
+                const loggedUser = result.user;
+                console.log(loggedUser)
+                navigate(from, { replace: true })
             })
             .catch(error => console.log(error))
     }
-    
+
     return (
         <div className='text-center'>
             <div className="divider">OR</div>
