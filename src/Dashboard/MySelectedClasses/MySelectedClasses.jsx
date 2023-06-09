6548f3eaaa333d23690e7e4c707079075bd2bbe2
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-06-09 12:36:06 +0600

    selcted pages table and data added

--- src/Dashboard/MySelectedClasses/MySelectedClasses.jsx
+++ src/Dashboard/MySelectedClasses/MySelectedClasses.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import useCartItems from '../../hooks/useCartItems';
+import { Helmet } from 'react-helmet-async';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MySelectedClasses = () => {
+    const [cartItems, isLoading, refetch] = useCartItems()
     return (
-        <div>
-            <p className='text-3xl text-center'>My Selected Classes Here</p>
+        <div className='w-full'>
+            <p className='text-3xl text-center'>My Selected Classes Here: {cartItems.length}</p>
+
+            <Helmet>
+                <title>Selected Classes | Cart | LS</title>
+            </Helmet>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Subject</th>
+                            <th>Instructor</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            cartItems?.map((carts, index) => <tr key={carts._id}>
+                                <th>{index + 1}</th>
+                                <td>{carts.subjectName}</td>
+                                <td>{carts.instructorName}</td>
+                                <td>$ {carts.courseFee}</td>
+                                <td><button
+                                    className='p-2 text-red-500 rounded-full shadow-lg hover:bg-accent-focus'
+                                ><FaTrashAlt></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
